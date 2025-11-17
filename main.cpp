b3777009527e71c17bb4ef15247c8ac9824f8fe7
Ata-Pab <atalay-pab@hotmail.com>
2025-11-17 14:16:31 +0300

    Add cpu_waster simple function as first example

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
-#include <iostream>
-#include <string>
 #include <thread>
 #include <chrono>
 #include <unistd.h>
+#include <iostream>
 
-int main() {
-    
-    std::cout << "Process ID: " << getpid() << std::endl;
-    return 0;
+// a simple function that wastes CPU cycles "forever"
+void cpu_waster()
+{
+    std::cout << "CPU Waster Process ID: " << getpid() << "\n";
+    std::cout << "CPU Waster Thread ID " << std::this_thread::get_id() << "\n";
+    while (true)
+        continue;
+}
+
+int main()
+{
+    std::cout << "Main Process ID: " << getpid() << "\n";
+    std::cout << "Main Thread ID: " << std::this_thread::get_id() << "\n";
+    std::thread thread1(cpu_waster);
+    std::thread thread2(cpu_waster);
+
+    while (true)
+    {
+        // keep the main thread alive "forever"
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 }
