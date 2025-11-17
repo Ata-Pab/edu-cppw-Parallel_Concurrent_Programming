f35b2a61210d95e486fddddb46a77ca7fca2b269
Ata-Pab <atalay-pab@hotmail.com>
2025-11-17 16:57:36 +0300

    Update main.cpp: Thread.joinable method introduced

--- main.cpp
+++ main.cpp
@@ -11,12 +11,15 @@ void chef_olivia() {
 int main() {
     std::cout << "Barron requests Olivia's help.\n";
     std::thread olivia(chef_olivia);
+    std::cout << "Olivia is Joinable?" << (olivia.joinable() ? " Yes\n" : " No\n");
 
     std::cout << "Barron continues cooking soup.\n";
     std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Olivia is Joinable?" << (olivia.joinable() ? " Yes\n" : " No\n");
 
     std::cout << "Barron patiently waits for Olivia to finish and join...\n";
     olivia.join(); // works like await in JS, waits for the thread to finish (barrier)
-
+    std::cout << "Olivia is Joinable?" << (olivia.joinable() ? " Yes\n" : " No\n");
+    
     std::cout << "Barron and Olivia are both done!\n";
 }
