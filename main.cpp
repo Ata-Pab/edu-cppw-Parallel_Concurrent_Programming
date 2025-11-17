ec170de41e203d9b2ea7d8007e05719d8ed5f0e2
Ata-Pab <atalay-pab@hotmail.com>
2025-11-17 16:00:06 +0300

    Update main.cpp: Thread.join method introduced

--- main.cpp
+++ main.cpp
@@ -1,27 +1,22 @@
 #include <thread>
 #include <chrono>
-#include <unistd.h>
 #include <iostream>
 
-// a simple function that wastes CPU cycles "forever"
-void cpu_waster()
-{
-    std::cout << "CPU Waster Process ID: " << getpid() << "\n";
-    std::cout << "CPU Waster Thread ID " << std::this_thread::get_id() << "\n";
-    while (true)
-        continue;
+void chef_olivia() {
+    std::cout << "Olivia started & waiting for sausage to thaw...\n";
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::cout << "Olivia is done cutting sausage.\n";
 }
 
-int main()
-{
-    std::cout << "Main Process ID: " << getpid() << "\n";
-    std::cout << "Main Thread ID: " << std::this_thread::get_id() << "\n";
-    std::thread thread1(cpu_waster);
-    std::thread thread2(cpu_waster);
+int main() {
+    std::cout << "Barron requests Olivia's help.\n";
+    std::thread olivia(chef_olivia);
 
-    while (true)
-    {
-        // keep the main thread alive "forever"
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
+    std::cout << "Barron continues cooking soup.\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::cout << "Barron patiently waits for Olivia to finish and join...\n";
+    olivia.join(); // works like await in JS, waits for the thread to finish (barrier)
+
+    std::cout << "Barron and Olivia are both done!\n";
 }
