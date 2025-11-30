efbca126761d9ba66c6dc74302eb9bc29c43fd36
Ata-Pab <atalay-pab@hotmail.com>
2025-11-30 12:18:51 +0300

    Add parallel_image_download.cpp file

---
+++ parallel_image_download.cpp
@@ -0,0 +1,18 @@
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <mutex>
+
+/*
+ * parallel_image_download.cpp
+ * Sort an array of random integers with merge sort
+ * This file is just for demonstration and test purposes.
+ * Source code for the actual implementations are in /examples folder.
+ *
+ * Reference: https://www.linkedin.com/learning/complete-guide-to-parallel-and-concurrent-programming-with-c-plus-plus/
+ */
+
+int main()
+{
+    
+}
